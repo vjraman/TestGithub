e574a572608483c8524dd7bb402ba52f701683c5
vijay <vijay.raman@gmail.com>
2016-01-02 00:19:01 -0500

    change test

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,6 @@
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Viji makes the save");
+        System.out.println("Viji uuuu makes the save");
     }
 }
